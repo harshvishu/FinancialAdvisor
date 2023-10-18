a5b5cf62e5c185369cb25e3c46b5f1e5023cf009
Harsh <harshv@nedbank.co.za>
2023-10-18 09:34:46 +0530

    Initial Commit

---
+++ FinancialAdvisor/FinancialAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAdvisorApp.swift
+//  FinancialAdvisor
+//
+//  Created by Harsh on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
